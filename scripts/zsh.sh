914c4c604f7d6e1299bb04965524571c57694d34
Tiago-Tamagusko <tamagusko@gmail.com>
2022-12-22 21:00:42 +0000

    add default .zshrc

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -38,4 +38,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sudo sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/' ~/.zshrc
-
